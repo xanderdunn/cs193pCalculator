72a35b3de25b954323cab7ea0150dcfbe49aa92c
Alex Dunn <thoughtadvances@icloud.com>
2012-12-07 21:12:19 +0545

    Add AxesDrawer library and enforce 80 character line limit, Add to do list for Assignment 3

--- Calculator/CalculatorBrain.h
+++ Calculator/CalculatorBrain.h
@@ -23,3 +23,24 @@
 + (NSString *)descriptionOfProgram:(id)program;
 
 @end
+
+// TO DO:
+// Create a new custom subclass of GraphViewController, GraphModel, and
+    // GraphView : UIView for the graph
+// Get these items on the storyboard.  Link the Graph button to a segue into
+    // the graphing screen
+// Implement GraphModel.m =
+// Implement pinching, panning, and triple-tapping
+// Save scale and origin information in NSUserDefaults
+// Convert to Universal App
+// New storyboard for iPad, new Views.  Rewire outlets
+
+// How do I access runProgram: usingVariables: from GraphView?
+// What is contained in GraphModel?  A collection of all points that need
+    // to be drawn?  Some kind of mapping between screen pixels and origin
+    // and axes?
+
+// Goals
+// Handful of lines added to CalculatorViewController
+// No changes to CalculatorBrain
+// <100 lines of code total added
