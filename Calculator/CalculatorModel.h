446599634c7578e310e1eb5227d53182bc39462e
Alex Dunn <thoughtadvances@icloud.com>
2012-12-07 22:45:10 +0545

    Rename from CalculatorBrain to CalculatorModel to maintain a standard

---
+++ Calculator/CalculatorModel.h
@@ -0,0 +1,25 @@
+//
+//  CalculatorModel.h
+//  Calculator
+//
+//  Created by admin on 27/N/12.
+//  Copyright (c) 2012 ThoughtAdvances. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorModel : NSObject
+- (void)pushOntoStack:(id)operand;
+- (void)clearStack;
+- (void)removeLastItem;
+- (NSString *)description;
+
+@property (readonly) id program;
+
++ (double)runProgram:(id)program;
++ (id)runProgram:(id)program
+        usingVariableValues:(NSDictionary *)variableValues;
++ (NSSet *)variablesUsedInProgram:(id)program;
++ (NSString *)descriptionOfProgram:(id)program;
+
+@end
